b191261398fb3c4e026c039b8675eea4c84ffb94
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-01-01 19:20:21 +0530

    theme switcher

--- airootfs/root/home/theme-switcher.sh
+++ airootfs/root/home/theme-switcher.sh
@@ -1,17 +1,39 @@
 #!/bin/env zsh
 
+# Directory where themes are stored
 THEME_DIR="$HOME/.config/themes"
 
+# Get a list of available themes
 themes=($(ls "$THEME_DIR"))
 
+# Show Wofi menu to select theme
 selected=$(printf "%s\n" "${themes[@]}" | wofi --dmenu --prompt "Select Theme")
 
+# Exit if no selection
 [ -z "$selected" ] && exit
 
-cp "$THEME_DIR/$selected/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
-cp "$THEME_DIR/$selected/waybar.conf" "$HOME/.config/waybar/config"
-cp "$THEME_DIR/$selected/wofi.rasi" "$HOME/.config/wofi/style.rasi"
+# Paths
+HYPR_CONFIG="$HOME/.config/hypr/modules/styling.conf"
+WAYBAR_CONFIG="$HOME/.config/waybar/config"
+WOFI_CONFIG="$HOME/.config/wofi/style.rasi"
 
+# Copy theme files
+if [[ -f "$THEME_DIR/$selected/styling.conf" ]]; then
+    cp "$THEME_DIR/$selected/styling.conf" "$HYPR_CONFIG"
+fi
+
+if [[ -f "$THEME_DIR/$selected/waybar.conf" ]]; then
+    cp "$THEME_DIR/$selected/waybar.conf" "$WAYBAR_CONFIG"
+fi
+
+if [[ -f "$THEME_DIR/$selected/wofi.rasi" ]]; then
+    cp "$THEME_DIR/$selected/wofi.rasi" "$WOFI_CONFIG"
+fi
+
+# Reload Hyprland and Waybar
 hyprctl reload
 killall waybar &>/dev/null
 waybar &
+
+# Optional: notify theme change
+notify-send "Theme switched" "$selected theme applied!"
