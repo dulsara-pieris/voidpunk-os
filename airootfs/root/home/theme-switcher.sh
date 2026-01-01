c5eb2577777a56f238e4833fe888693f39d6dbb2
dulsara-pieris <dulsara.synapsnex@gmail.com>
2026-01-01 19:08:37 +0530

    theme switcher

--- airootfs/root/home/theme-switcher.sh
+++ airootfs/root/home/theme-switcher.sh
@@ -1,22 +1,17 @@
-#!/bin/bash
+#!/bin/env zsh
 
 THEME_DIR="$HOME/.config/themes"
 
-# Get list of themes
 themes=($(ls "$THEME_DIR"))
 
-# Show menu with Wofi
 selected=$(printf "%s\n" "${themes[@]}" | wofi --dmenu --prompt "Select Theme")
 
-# Exit if no selection
 [ -z "$selected" ] && exit
 
-# Copy theme files to main config
 cp "$THEME_DIR/$selected/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
 cp "$THEME_DIR/$selected/waybar.conf" "$HOME/.config/waybar/config"
 cp "$THEME_DIR/$selected/wofi.rasi" "$HOME/.config/wofi/style.rasi"
 
-# Reload Hyprland and Waybar
 hyprctl reload
 killall waybar &>/dev/null
 waybar &
